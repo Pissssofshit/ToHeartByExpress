ce9b6e957c06e0638b81a83ea9fc826dbd819494
sasuke1go <804214023@qq.com>
2017-05-18 17:05:06 +0800

    完成了3个frament之间的切换

--- app/src/main/java/cn/zucc/qifeng/toheartbyexpress/MainActivity.java
+++ app/src/main/java/cn/zucc/qifeng/toheartbyexpress/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabReselectListener;
@@ -35,43 +36,50 @@ public class MainActivity extends AppCompatActivity {
 
 
         fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
 
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
+                fragmentTransaction = fragmentManager.beginTransaction();//这个只能提交一次 注意！！
+                hideallframgment(fragmentTransaction);
                 switch (tabId) {
                     case R.id.tab_map:
-                        Log.d(TAG, "hi ");
-                        if (mapFragment==null)
+                        if (mapFragment == null) {//如果没有创建过就 实例化一个，并add到ft中 ，提交后显示
                             mapFragment = new MapFragment();
-
-                        fragmentTransaction.add(R.id.main_fragment, mapFragment);
-
-                        fragmentTransaction.hide(mapFragment);
-                        fragmentTransaction.show(mapFragment);
-                        fragmentTransaction.commit();
+                            fragmentTransaction.add(R.id.main_fragment, mapFragment);
+                        } else fragmentTransaction.show(mapFragment);
                         break;
                     case R.id.tab_share:
-
+                        if (shareFragment == null) {
+                            shareFragment = new ShareFragment();
+                            fragmentTransaction.add(R.id.main_fragment, shareFragment);
+                        } else fragmentTransaction.show(shareFragment);
                         break;
                     case R.id.tab_me:
-
+                        if (meFragment == null) {
+                            meFragment = new MeFragment();
+                            fragmentTransaction.add(R.id.main_fragment, meFragment);
+                        } else fragmentTransaction.show(meFragment);
                         break;
-
                 }
+                fragmentTransaction.commit();
             }
         });
+        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(@IdRes int tabId) {
+                Toast.makeText(MainActivity.this, "小哥哥我已经在了别点了", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void hideallframgment(FragmentTransaction fragmentTransaction) {
+        if (mapFragment != null) fragmentTransaction.hide(mapFragment);
+        if (shareFragment != null) fragmentTransaction.hide(shareFragment);
+        if (meFragment != null) fragmentTransaction.hide(meFragment);
 
     }
-//    if (meFragment == null)//map_fragmen没有创建过
-//    {
-//        meFragment = new MeFragment();
-//        fragmentTransaction.add(R.id.main_fragment, meFragment);
-//    } else {
-//        fragmentTransaction.show(meFragment);
-//    }
-//                        fragmentTransaction.commit();
+
 }
