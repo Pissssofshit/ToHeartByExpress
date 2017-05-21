4f198be0abbb4732c3fe6b52843ca58bc75515e4
sasuke1go <804214023@qq.com>
2017-05-21 17:29:01 +0800

    finsh three tab swtich

--- app/src/main/java/cn/zucc/qifeng/toheartbyexpress/MeFragment.java
+++ app/src/main/java/cn/zucc/qifeng/toheartbyexpress/MeFragment.java
@@ -21,51 +21,6 @@ public class MeFragment extends Fragment {
 
         return view;
     }
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Log.d(TAG, "MeFragment+onActivityCreated: ");
-    }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "MeFragment+onStart: ");
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d(TAG, "MeFragment+onResume: ");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "MeFragment+onPause: ");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d(TAG, "MeFragment+onStop: ");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Log.d(TAG, "MeFragment+onDestroyView: ");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "MeFragment+onDestroy: ");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        Log.d(TAG, "MeFragment+onDetach: ");
-    }
 }
