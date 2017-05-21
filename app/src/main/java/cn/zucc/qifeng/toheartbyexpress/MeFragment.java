baef79d625716986df0fd810a60c350d941dfaa9
sasuke1go <804214023@qq.com>
2017-05-21 20:35:06 +0800

    关于我的界面的 雏形

--- app/src/main/java/cn/zucc/qifeng/toheartbyexpress/MeFragment.java
+++ app/src/main/java/cn/zucc/qifeng/toheartbyexpress/MeFragment.java
@@ -1,12 +1,23 @@
 package cn.zucc.qifeng.toheartbyexpress;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 80421 on 2017/5/17.
@@ -14,13 +25,19 @@ import android.view.ViewGroup;
 
 public class MeFragment extends Fragment {
     private static final String TAG="MeFragementp";
+    private String[] data={"我的空间","我的好友", "地址管理","检查更新"};
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_me,container,false);
-
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,data);
+        ListView listview= (ListView) view.findViewById(R.id.melist);
+        listview.setAdapter(adapter);
         return view;
     }
+
+
 
 
 }
