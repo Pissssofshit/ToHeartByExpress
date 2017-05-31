7f4da07e966e6502a373ffdf5c4613ef117dc1e3
Pissssofshit <1412143367@qq.com>
2017-05-31 13:18:12 +0800

    poikeywordsearch

--- app/src/main/java/cn/zucc/qifeng/toheartbyexpress/itemOfHomepage/PublishTask.java
+++ app/src/main/java/cn/zucc/qifeng/toheartbyexpress/itemOfHomepage/PublishTask.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import cn.zucc.qifeng.toheartbyexpress.R;
+import cn.zucc.qifeng.toheartbyexpress.gdmap.gdmap.poisearch.PoiKeywordSearchActivity;
 
 public class PublishTask extends AppCompatActivity {
     public static void Start(Context context){
@@ -16,5 +21,25 @@ public class PublishTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish_task);
+        Button button1= (Button) findViewById(R.id.start_map);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(PublishTask.this, PoiKeywordSearchActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK)
+        {
+            String address=data.getStringExtra("address");
+            Log.d("publish", address);
+            TextView text= (TextView) findViewById(R.id.address);
+            text.setText(address);
+        }
     }
 }
